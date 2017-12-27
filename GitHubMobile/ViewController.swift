09dc37c114ef4ec9c47c37f0bac249da97da1eda
zizi4n5 <zizi4n5@gmail.com>
2017-12-27 19:31:38 +0900

    GitHubMobileプロジェクトを追加（Single View Appテンプレート）

---
+++ GitHubMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubMobile
+//
+//  Created by zizi4n5 on 2017/12/27.
+//  Copyright © 2017年 zizi4n5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
