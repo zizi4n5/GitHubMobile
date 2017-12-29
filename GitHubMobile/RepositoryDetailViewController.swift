a4b6e49dae3e59deb42d09ab73cdcdca23555ec9
zizi4n5 <zizi4n5@gmail.com>
2017-12-30 07:56:58 +0900

    リファクタリング ・不要なコメントの削除 ・冗長な処理を修正

--- GitHubMobile/RepositoryDetailViewController.swift
+++ GitHubMobile/RepositoryDetailViewController.swift
@@ -17,29 +17,11 @@ class RepositoryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         let request = URLRequest(url: url)
         webview.loadRequest(request)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- GitHubMobile/RepositoryTableViewController.swift
+++ GitHubMobile/RepositoryTableViewController.swift
@@ -17,20 +17,19 @@ class RepositoryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.estimatedRowHeight = 64
-        self.tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 64
+        tableView.rowHeight = UITableViewAutomaticDimension
         
-        GitHubClient.default.getRepositories(first: 10)  { (repositories, error) in
+        reloadRepositories()
+    }
+    
+    func reloadRepositories() {
+        GitHubClient.default.getRepositories(first: 50)  { (repositories, error) in
+            self.repositories.removeAll()
             self.repositories.append(contentsOf: repositories)
             self.numberOfRowsInSection = self.repositories.count
             self.tableView.reloadData()
         }
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,50 +69,10 @@ class RepositoryTableViewController: UITableViewController {
         performSegue(withIdentifier: "OpenRepositoryDetail", sender: repositories[indexPath.row].url)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        let detailVC = segue.destination as! RepositoryDetailViewController
-        
-        if let url = sender as? URL {
+        if let detailVC = segue.destination as? RepositoryDetailViewController, let url = sender as? URL {
             detailVC.url = url
         }
     }
